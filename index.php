b7b101856e696923b1130a29751972e4a300d04b
AmirAli <amiralidev20@gmail.com>
2022-08-29 15:58:35 +0430

    start project

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php include 'section/header.php';?>
+
+
+
+<header class="header">
+        <section class="header-top">
+            <div class="container">
+                <div class="header-wrapper">
+                    <div class="header-logo">
+                        <a href="index.php" class="header-logo__link">Blogs</a>
+                    </div>
+                    <nav class="menu">
+
+                        <li class="menu__item"><a class="menu-item__link" href="">HOME</a></li>
+                        <li class="menu__item"><a class="menu-item__link" href="">ABOUT US</a></li>
+                        <li class="menu__item"><a class="menu-item__link" href="">FIND BLOGS</a></li>
+                        <li class="menu__item"><a class="menu-item__link" href="">SIGN UP</a></li>
+                        <li class="menu__item"><a class="menu-item__link" href="">LOGIN IN</a></li>
+                    </nav>
+                </div>
+
+            </div>
+        </section>
+
+
+
+        <section class="header-bottom">
+            <div class="container">
+                <div class="header-bottom__wrapper">
+                    <div class="header-title">
+                        <h2>This is An Introduction</h2>
+                        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam officia aliquid maxime nisi dolore laboriosam nostrum libero sequi unde .</span>
+                    </div>
+
+                    <div class="header-content">
+                        <h2 class="header-content__title">Some Basic Cateogories</h2>
+                        <div class="header-boxs">
+                            <div class="header-box">
+                                Fun Stuff
+                            </div>
+                            <div class="header-box ">
+                                Serious Stuff
+                            </div>
+                            <div class="header-box">
+                                Exciting Stuff
+                            </div>
+                            <div class="header-box">
+                                Boring Stuff
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+
+
+    </header>
+<?php include 'section/footer.php';?>
---
+++ section/footer.php
@@ -0,0 +1,11 @@
+
+
+    <footer class="footer">
+        <div class="container">
+            copyright &COPY; by AmirAli 2022
+        </div>
+    </footer>
+
+</body>
+
+</html>
